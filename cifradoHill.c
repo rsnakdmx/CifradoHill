cde36a08972cae483e6a091588446aa2206a262b
Miguel Rubi <rsnakdmx@hotmail.com>
2019-05-27 12:45:29 -0500

    Version paralelizada

--- cifradoHill.c
+++ cifradoHill.c
@@ -1,111 +1,164 @@
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct text
 {
     int size;
-    char *body;
+    char * restrict body;
 }Text;
 
 typedef struct node
 {
     int data;
-    struct node *next;
-    struct node *prev;
+    struct node * restrict next;
+    struct node * restrict prev;
 }Node;
 
-typedef struct stack
+typedef struct queue
 {
     int size;
-    Node *bottom;
-    Node *top;
-}Stack;
+    Node * restrict bottom;
+    Node * restrict top;
+}Queue;
 
-Stack *createStack();
-void push(Stack *, int);
-int pop(Stack *);
-Text* readFile(FILE *);
-void charToInt(Stack *, Text *);
+static Queue * restrict queue = NULL;
+static int key[3][3] = { {1, 2, 3},	{0, 4, 5}, {1, 0, 6} };
 
-int main()
+Queue *createStack();
+void enqueue(int);
+int dequeue();
+Text *readFile(FILE *);
+void charToInt(Text *);
+int *cipherText();
+
+int main(int argc, char *argv[])
 {
-    Text *ptr = readFile(fopen("lorem.txt", "rb"));
-    Stack *stack = NULL;
-    stack = createStack();
-    charToInt(stack, ptr);
+	if (argc == 2)
+	{
+		Text * restrict ptr = readFile(fopen("lorem.txt", "rb"));
+    	queue = createStack();
+		int * restrict tmp = NULL;
 
-    printf(" %d\n", pop(stack));
-    printf(" %d\n", pop(stack));
-    // for (int i = 0; i < ptr -> size; i++)
-    //     printf("%d ", toX[i]);
-    //printf("File size: %d\n", ptr -> size);
-    //printf("%s", ptr -> body);
-    free(ptr -> body);
-    free(ptr);
-    return 0x0;
+    	charToInt(ptr);
+	
+		while (queue -> size > 0)
+		{
+			tmp = cipherText();
+
+			for (int idx = 0; idx < 3; idx++)
+				printf("%c", (char)(tmp[idx]) + 65);
+		
+			free(tmp);
+			tmp = NULL;
+		}
+
+		printf("\n");
+    	free(ptr -> body);
+    	free(ptr);
+    	return EXIT_SUCCESS;
+	}
+
+	else
+	{
+		printf("Especifica el archivo a cifrar\n");
+		return EXIT_FAILURE;
+	}
+	
 }
 
-Text *readFile(FILE *ap)
+Text *readFile(FILE * restrict ap)
 {
-    Text *block = (Text*)calloc(0x1, sizeof(Text));
+    Text * restrict block = (Text * restrict)calloc(1, sizeof(Text));
 
     fseek(ap, 0, SEEK_END);
     block -> size = ftell(ap);
     rewind(ap);
-    block -> body = (char*)calloc(block -> size, sizeof(char));
+    block -> body = (char * restrict)calloc(block -> size, sizeof(char));
     fread(block -> body, sizeof(char), block -> size, ap);
     fclose(ap);
     return block;
 }
 
-void charToInt(Stack *stack, Text *block)
+void charToInt(Text * restrict block)
 {
-    for (int idx = 0; idx < block -> size; idx++)
+    for (register int idx = 0; idx < block -> size; idx++)
     {
         if (block -> body[idx] < 0 && block -> body[idx + 1] < 0)
-            push(stack, 13);
-
+            enqueue(13);
+	
         else if (block -> body[idx] > 64 && block -> body[idx] < 91)
-            push(stack, (int)(block -> body[idx] - 65));
+            enqueue((int)(block -> body[idx] - 65));
         
         else if (block -> body[idx] > 96 && block -> body[idx] < 123)
-            push(stack, (int)(block -> body[idx] - 97));
+            enqueue((int)(block -> body[idx] - 97));
     }
 }
 
-Stack *createStack()
+Queue *createStack()
 {
-    return (Stack*)calloc(0x1, sizeof(Stack));
+    return (Queue * restrict)calloc(1, sizeof(Queue));
 }
 
-void push(Stack *stack, int val)
+void enqueue(int val)
 {
-    Node *new = (Node*)calloc(0x1, sizeof(Node));
+    Node * restrict new = (Node * restrict)calloc(1, sizeof(Node));
     new -> data = val;
 
-    if (!stack -> bottom)
+    if (!queue -> bottom)
     {
-        stack -> bottom = new;
-        stack -> top = stack -> bottom;
+        queue -> bottom = new;
+        queue -> top = queue -> bottom;
     }
 
     else
     {
-        stack -> top -> next = new;
-        stack -> top -> prev = stack -> top;
-        stack -> top = stack -> top -> next;
+        new -> prev = queue -> top;
+        queue -> top -> next = new;
+        queue -> top = queue -> top -> next;
     }
 
-    stack -> size++;
+    queue -> size++;
 }
 
-int pop(Stack *stack)
+int dequeue()
 {
-    Node *tmp = stack -> top;
-    int val = stack -> top -> data;
+	if (queue -> size > 0)
+	{
+		int val = queue -> bottom -> data;
+    	Node * restrict tmp = queue -> bottom;
 
-    stack -> size--;
-    printf("%p", stack -> top);
-    free(tmp); 
-    return val;
+    	queue -> bottom = queue -> bottom -> next;
+    	queue -> size--;
+    	free(tmp);
+
+    	return val;
+	}
+
+	else
+		return 23;
+}
+
+int *cipherText()
+{
+	int tmp[3] = {0, 0, 0};
+	int *ret = (int * restrict)calloc(3, sizeof(int));
+
+	for (int idx = 0; idx < 3; idx++)
+		tmp[idx] = dequeue();
+
+	omp_set_num_threads(3);
+	#pragma omp parallel
+	{
+		for(int idx = 0; idx < 3; idx++)
+		{
+			#pragma omp for
+			for (int jdx = 0; jdx < 3; jdx++)
+				ret[idx] += key[idx][jdx] * tmp[jdx];
+		
+			ret[idx] = ret[idx] % 26;
+		}
+	}
+
+	return ret;
 }
